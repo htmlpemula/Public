64afb43d5df9e422f9686a7799362f8fe759b67e
iqbalmuhammad-big <iqbalmuhammadadiatma@gmail.com>
2021-04-13 11:30:04 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ }, 1250, 'easeInOutExpo');
+
+ e.preventDefault();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
